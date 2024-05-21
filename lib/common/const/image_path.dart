91b1c4f52f6ddfb0c957ce1f553201197072484d
yeongwoo <jyy0223@naver.com>
2024-05-21 21:29:17 +0900

    [feat] image_path 추가

---
+++ lib/common/const/image_path.dart
@@ -0,0 +1,19 @@
+class ImagePath {
+  /// img
+  static const logo = "asset/img/logo/logo.png";
+
+  /// social_login button
+  static const appleIcon = "asset/img/social_login/button/apple.png";
+  static const googleIcon = "asset/img/social_login/button/google.png";
+  static const kakaoIcon = "asset/img/social_login/button/kakao.png";
+  static const naverIcon = "asset/img/social_login/button/naver.png";
+
+  /// social_login screen
+  static const appleScreen = "asset/img/social_login/screen/apple.png";
+  static const googleScreen = "asset/img/social_login/screen/google.png";
+  static const kakaoScreen = "asset/img/social_login/screen/kakao.png";
+  static const naverScreen = "asset/img/social_login/screen/naver.png";
+
+  /// user
+  static const user = "asset/img/user/user.png";
+}
