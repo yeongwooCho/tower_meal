7757705f5d3100b76db28804a8d6a8d09a7619d7
yeongwoo <jyy0223@naver.com>
2024-05-21 21:27:43 +0900

    [feat] 텍스트 스타일 추가

---
+++ lib/common/const/text_styles.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class MyTextStyle {
+  static const letterSpacing = -1.025;
+  static const height = 1.2;
+  static const basicColor = MyColor.text;
+
+  static const headTitle = TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bigTitleBold = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bigTitleMedium = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bigTitleRegular = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bodyTitleBold = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bodyTitleMedium = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bodyTitleRegular = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bodyBold = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bodyMedium = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const bodyRegular = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const descriptionBold = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const descriptionMedium = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const descriptionRegular = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+
+  static const minimumRegular = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    letterSpacing: letterSpacing,
+    height: height,
+    color: basicColor,
+  );
+}
