9469f1fbdbce3415d25458cf2070e9a692372fba
yeongwoo <jyy0223@naver.com>
2024-05-24 16:24:14 +0900

    [feat] consulting_provider 구현

---
+++ lib/second/consulting/provider/consulting_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tower_meal/common/utils/data_utils.dart';
+import 'package:tower_meal/second/consulting/model/concept_model.dart';
+import 'package:tower_meal/second/consulting/model/consulting_model.dart';
+import 'package:tower_meal/second/consulting/model/manufacturing_model.dart';
+
+final consultingProvider =
+    StateNotifierProvider<ConsultingStateNotifier, ConsultingModel>((ref) {
+  return ConsultingStateNotifier();
+});
+
+class ConsultingStateNotifier extends StateNotifier<ConsultingModel> {
+  ConsultingStateNotifier()
+      : super(ConsultingModel(
+          id: '',
+          status: ConsultingStatus.doneManufacture,
+        ));
+
+  void updateConcept({
+    required ConceptModel concept,
+  }) {
+    state = ConsultingModel(
+      id: DataUtils.getUuid(),
+      concept: concept,
+      manufacturing: state.manufacturing,
+      status: state.status,
+    );
+  }
+
+  void updateManufacturing({
+    required ManufacturingModel manufacturing,
+  }) {
+    state = ConsultingModel(
+      id: DataUtils.getUuid(),
+      concept: state.concept,
+      manufacturing: manufacturing,
+      status: state.status,
+    );
+  }
+}
